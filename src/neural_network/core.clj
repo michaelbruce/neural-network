57f7ad89957f3c48c5fab3dc3f2a7ca401031227
Michael Bruce <michael.bruce@singletracksystems.com>
2016-05-26 00:19:09 +0100

    extracted location addressing to synapse function

--- src/neural_network/core.clj
+++ src/neural_network/core.clj
@@ -1,21 +1,5 @@
 (ns neural-network.core)
 
-;(defn backward-propogation [x])
-
-(defn exp [x n]
-  (reduce * (repeat n x)))
-
-(defn sigmoid [x]
-  (/ 1 (+ 1 (exp 1 (- x)))))
-
-(defn neuron [& inputs]
-  "Performs a calculation given inputs"
-  (sigmoid (reduce + inputs)))
-
-(defn synapse [weight input]
-  "Applys a weight to an input"
-  (* weight input))
-
 (defonce initial-inputs [1 1])
 
 (defonce synapses (atom {:first-layer {:1 0.8
@@ -28,18 +12,34 @@
                                        :2 0.5
                                        :3 0.9}}))
 
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+(defn sigmoid [x]
+  (/ 1 (+ 1 (exp 1 (- x)))))
+
+(defn neuron [& inputs]
+  "Performs a calculation given inputs"
+  (sigmoid (reduce + inputs)))
+
+(defn synapse [location input]
+  "Applys a weight to an input"
+  (let [weight ((second location)
+                ((first location) @synapses))]
+    (* weight input)))
+
 (defn network [inital-inputs]
   "Network based on miller's first example."
   (neuron
-    (synapse (:1 (:second-layer @synapses))
+    (synapse [:second-layer :1]
              (neuron
-               (synapse (:1 (:first-layer @synapses)) (first inital-inputs))
-               (synapse (:2 (:first-layer @synapses)) (second inital-inputs))))
-    (synapse (:2 (:second-layer @synapses))
+               (synapse [:first-layer :1] (first inital-inputs))
+               (synapse [:first-layer :2] (second inital-inputs))))
+    (synapse [:second-layer :2]
              (neuron
-               (synapse (:3 (:first-layer @synapses)) (first inital-inputs))
-               (synapse (:4 (:first-layer @synapses)) (second inital-inputs))))
-    (synapse (:3 (:second-layer @synapses))
+               (synapse [:first-layer :3] (first inital-inputs))
+               (synapse [:first-layer :4] (second inital-inputs))))
+    (synapse [:second-layer :3]
              (neuron
-               (synapse (:5 (:first-layer @synapses)) (first inital-inputs))
-               (synapse (:6 (:first-layer @synapses)) (second inital-inputs))))))
+               (synapse [:first-layer :5] (first inital-inputs))
+               (synapse [:first-layer :6] (second inital-inputs))))))
